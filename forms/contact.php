bc78e7664d5028384e9ef6ce438f037e2d6e486d
José María <hbjosemaria@gmail.com>
2020-11-09 20:35:45 +0000

    Commit para cambiar el mail al que se envia el formulario de contacto

--- forms/contact.php
+++ forms/contact.php
@@ -7,12 +7,12 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'hbjosemaria@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
+    die( 'No se pudo cargar la libreria php-email-form');
   }
 
   $contact = new PHP_Email_Form;
